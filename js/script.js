aaeb84640dc97025029e9d4dcca74d7bb13f0cfc
Jakub Przybysz <Jakub.Przybysz@tlx.local>
2017-11-21 13:54:27 +0100

    images

--- js/script.js
+++ js/script.js
@@ -9,12 +9,22 @@
             name: 'Dodecahedron',
             price: 2.95,
             description: 'As you can see, there is a TODO in the code. Let’s implement the missing mapping from request URLs to mock data. This can be done with MockConnection from the package @angular/http/testing. We check requested URLs to the fake backend and return (modified) mock data. Requests, we are not interested in, can be passed through (real remote calls).',
+            image:
+                {
+                    full: 'img/image-full.png',
+                    thumb: 'img/image-thumb.png',
+                },
             canPurchase: true,
             soldOut: true,
         },
         {
             name: "Pentagonal Gem",
             price: 5.95,
+            image:
+                {
+                    full: 'img/image-full.png',
+                    thumb: 'img/image-thumb.png',
+                },
             description: 'The mock Http service can be implemented with a factory method. We just need to use the following fakeBackendFactory method in place of standard Http service.',
             canPurchase: true,
             soldOut: true,
@@ -22,6 +32,11 @@
         {
             name: "Normal Gem",
             price: 4.35,
+            image:
+                {
+                    full: 'img/image-full.png',
+                    thumb: 'img/image-thumb.png',
+                },
             description: 'Before implementing a fake or mock Http service, we need data. Let’s create a mock database as simple array of employees objects.',
             canPurchase: true,
             soldOut: true,
@@ -29,6 +44,11 @@
         {
             name: "Octagon Gem",
             price: 8.65,
+            image:
+                {
+                    full: 'img/image-full.png',
+                    thumb: 'img/image-thumb.png',
+                },
             description: 'The domain model object Employee is defined by the following interface.',
             canPurchase: true,
             soldOut: true,
@@ -36,6 +56,11 @@
         {
             name: "Figure",
             price: 1.99,
+            image:
+                {
+                    full: 'img/image-full.png',
+                    thumb: 'img/image-thumb.png',
+                },
             description: 'Welcome to our Game Update! It’s been great seeing all the feedback you’re providing for our game!',
             canPurchase: true,
             soldOut: true,
@@ -43,6 +68,11 @@
         {
             name: "Non Gem",
             price: 2.45,
+            image:
+                {
+                    full: 'img/image-full.png',
+                    thumb: 'img/image-thumb.png',
+                },
             description: 'Thank you all for your patience as we continue to optimize the game for PC and enforce in-game security.',
             canPurchase: true,
             soldOut: true,
